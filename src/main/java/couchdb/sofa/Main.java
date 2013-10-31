9561d515adf87629c57661cce80433cfc996705d
Michał Suchecki <michal.suchecki@javart.eu>
2013-10-31 14:31:00 +0100

    1

--- src/main/java/couchdb/sofa/Main.java
+++ src/main/java/couchdb/sofa/Main.java
@@ -1,11 +1,6 @@
 package couchdb.sofa;
 
-import com.google.common.collect.Lists;
-import org.ektorp.Options;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Date;
-import java.util.HashMap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,34 +19,14 @@ public class Main {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("couchdb-and-java.xml");
 
         SofaRepository sofaRepository = (SofaRepository) context.getBean("sofaRepository");
-//
-//        Sofa sofa = Sofa.SofaBuilder.sofa()
-//                .withColor("black")
-//                .build();
-//
-//
-//        sofaRepository.add(sofa);
 
-        Options options = new Options().param("paramName","paramValue");
-        Sofa sofa = sofaRepository.get("20e067cfe38852a9f4433dbe050001ed");
-
-        System.out.println(sofa);
-
-        sofa.setSeats(3);
-        sofa.setDateCreated(new Date());
-        sofa.setImageURLs(Lists.newArrayList("ala", "ma", "kota"));
-        HashMap<String, Pillow> pillows = new HashMap<String, Pillow>();
-        pillows.put("pillow 1", new Pillow(Pillow.Softness.FIRM));
-        pillows.put("pillow 2", new Pillow(Pillow.Softness.FIRM));
-        pillows.put("pillow 3", new Pillow(Pillow.Softness.FIRM));
-        sofa.setPillows(pillows);
-
-        sofaRepository.update(sofa);
+        Sofa sofa = Sofa.SofaBuilder.sofa()
+                .withColor("black")
+                .build();
 
 
-//        Sofa sofa = sofaRepository.get("20e067cfe38852a9f4433dbe050001ed", new Options().includeRevisions());
-//        System.out.println(sofa);
-//        System.out.println(sofa.getRevisions());
+        sofaRepository.add(sofa);
+
 
     }
 
